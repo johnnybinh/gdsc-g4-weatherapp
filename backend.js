a1a5af1bf87619ad6ecfa03b5adb9c153da47d6f
johnnybinh <thebinhnguyen2703@gmail.com>
2023-12-02 15:23:45 +0700

    change to celsius

--- backend.js
+++ backend.js
@@ -3,14 +3,14 @@ const apiKey = "c6a2c9d2213e2e27c40c33eaef674179"
 var city;
 
 function logWeather(){
-  fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + apiKey).then(response => response.json())
+  fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid=" + apiKey).then(response => response.json())
   .then(data => {
     console.log(JSON.stringify(data));
-    temp = data.main.temp;
+    var temp = data.main.temp;
     console.log(temp);
     document.getElementById("pic").src = "https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
     document.getElementById("cityname").innerHTML = data.name;
-    document.getElementById("temp").innerHTML = "Current Temp is: "+ data.main.temp+"°F";
+    document.getElementById("temp").innerHTML = "Current Temp is: "+ temp +"°C";
     document.getElementById("description").innerHTML = "Current Weather:"+ data.weather[0].description;
   })
 }
